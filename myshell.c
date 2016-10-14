dbf34d16bc816f7dbb89bf6db9302c87c91c9384
Colin Barnes <colin.barnes@temple.edu>
2016-10-13 19:08:53 -0700

    cleaned up some old code

--- myshell.c
+++ myshell.c
@@ -13,8 +13,6 @@ void list_cur_dir(char *buffer[]);
 int is_io_redirect(char *argv[], int argc, int *in, int *out);
 int handle_io_redirect(char *argv[], int *in_index, int *out_index);
 
-//void print_environ(extern char **envi);
-//void print_cwd(char *buffer, int size);
 
 extern char **environ;
 
@@ -42,23 +40,6 @@ int main(int argc, char *argv[])
 		char *cwd = getcwd(cwd_buffer, cwd_size);
 		//printf("Current Working Directory: %s\n", cwd);
 		
-		/* SET THE NEW shell ENVIRONMENT VARIABLE */
-		/* NOT WORKING
-		char env_str[80];
-	  	strcpy (env_str,"shell=");
-	  	strcpy (env_str, cwd);
-	  	strcat (env_str,"/myshell");
-	  	//printf("env_str: %s\n", env_str);
-		int env_err = putenv(env_str);
-		//printf("env_err: %d\n", env_err);
-		
-		int zz;
-		for (zz=0; environ[zz]!=NULL; zz++) {
-			//printf("%s\n", environ[zz]);
-		}	
-		char *a = getenv(env_str);
-		//printf("char a: %s\n", a);
-		*/
 
 		// PRINT PROMPT TO USER AND GET USER STRING
 		printf("%smyshell>>", cwd);
@@ -66,7 +47,6 @@ int main(int argc, char *argv[])
 		{
 			exit(0);
 		}
-		//printf("user input string: %s\n", user_input_buffer);
 		
 		
 		char user_input_buffer2[100] = "";
@@ -86,12 +66,7 @@ int main(int argc, char *argv[])
 		char *argv[100];
 		
 		argc = parse(user_input_buffer2, argv);
-		//printf("argc: %d\n", argc);
-		int i;
-		for (i = 0; i <= argc; i++)
-		{
-			//printf("argv[%d]: %s\n",i, argv[i]); 
-		}
+
 		
 		if ( !strcmp(argv[0], "exit") )
 			exit(0);
@@ -112,9 +87,7 @@ int main(int argc, char *argv[])
 		
 			if ( is_relative_pathname(argv[0]) )
 			{
-				//printf("is absolute pathname!\n");
 				abs_pathname = argv[0];
-				//printf("absolute pathname: %s\n", abs_pathname);
 			}
 		
 			// CREATE A NEW PROCESS AND EXECUTE USER COMMAND (IN THE CURRENT WORKING DIRECTORY ONLY)
@@ -132,38 +105,11 @@ int main(int argc, char *argv[])
 				
 				if ( is_io_redirect(argv, argc, in_index, out_index) > 0 )		// std input char '<' encountered
 				{
-					//printf("in_index: %d\n", *in_index);
-					//printf("out_index: %d\n", *out_index);
 					handle_io_redirect(argv, in_index, out_index);
 				}				
-				/*
-				if ( is_io_redirect(argv, argc) == 2 )		// std output char '>' encountered
-				{
-					int newstdout = open(argv[2], O_WRONLY|O_CREAT,S_IRWXU|S_IRWXG|S_IRWXO);
-					close(1);
-					dup(newstdout);
-					close(newstdout);
-									
-				}
-				else if ( is_io_redirect(argv, argc) == 1 )		// std input char '<' encountered
-				{
-					int	newstdin = open(argv[2], O_RDONLY);
-					close(0);
-					dup(newstdin);
-					close(newstdin);
-					
-				}
-				else if ( is_io_redirect(argv, argc) == 3 )		// '<' ... '>'
-				{
-					;
-				}
-				else if ( is_io_redirect(argv, argc) == 4 )		// '>' ... '<'
-				{
-					;
-				}
-				*/
+
 				char *child_argv[] = {abs_pathname, NULL };
-				//if ( (execvp(abs_pathname, child_argv)) < 0 )
+
 				if ( (execvp(argv[0], argv)) < 0 )
 				{
 					printf("Error while executing command: The specified executable does not exist\n");
@@ -247,29 +193,24 @@ int handle_internal_cmd(char *argv[])
 {
 	if ( !strcmp(argv[0], "cd") )		// working
 	{
-		//printf("cd\n");
 		my_chdir(argv);
 	}
 	else if ( !strcmp(argv[0], "clr") )		// working
 	{
-		//printf("clr\n");
 		system("clear");
 	}
 	else if ( !strcmp(argv[0], "dir") )		// working
 	{
-		//printf("dir\n");
 		char *dir_buffer[100];
 		list_cur_dir(dir_buffer);
 	}
 	else if ( !strcmp(argv[0], "environ") )		//working
 	{
-		//printf("environ\n");
 		char *path = getenv("PATH");
 		printf("PATH : %s\n", path);
 	}	
 	else if ( !strcmp(argv[0], "echo") )	// semi-working but does not work for spaces
 	{
-		//printf("echo\n");
 		int i = 1;
 		while (argv[i])
 		{
@@ -445,28 +386,6 @@ int handle_io_redirect(char *argv[], int *in_index, int *out_index)
 	
 	return err_flag;
 }
-
-/*
-void print_environ(extern char **envi)
-{
-
-	int i;
-	for (i = 0; environ[i]!=NULL; i++)
-	{
-		printf("environ[%d]: %s\n", i, environ[i]);
-	}
-
-}
-*/
-/*	NOT CURRENTLY USING THIS
-void print_cwd(char *buffer, int size)
-{
-	size_t size1 = size;
-	//printf("buffer: %s, size: %zu\n", buffer, size1);
-	char *cwd = getcwd(buffer, size1);
-	printf("Current Working Directory: %s\n", cwd);
-}
-*/
 
 
 
